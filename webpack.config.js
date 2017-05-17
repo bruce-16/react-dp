938efb31bcaebe98e8586c96ed7ad8bacb085a0a
administrator <administrator@shangdedifangdifang.local>
2017-05-17 15:12:38 +0800

    删除无用项

--- webpack.config.js
+++ webpack.config.js
@@ -99,36 +99,31 @@ module.exports = {
         //     })
         //   ];
         // }
-
-        /*eslint: function(){
-         configFile: '.eslintrc' // Rules for eslint
-         },*/
       }
     }),
 
 
     // 打开浏览器
     new OpenBrowserPlugin({
-      url: 'http://localhost:8080'
+      url: 'http://localhost:8888'
     })
 
 
   ],
 
-  // devServer: {
-  //   proxy: {
-  //     // 凡是 `/api` 开头的 http 请求，都会被代理到 localhost:3000 上，由 koa 提供 mock 数据。
-  //     // koa 代码在 ./mock 目录中，启动命令为 npm run mock
-  //     '/api': {
-  //       target: 'http://localhost:3000',
-  //       secure: false
-  //     }
-  //   },
-  //   contentBase: "./public", //本地服务器所加载的页面所在的目录
-  //   colors: true, //终端中输出结果为彩色
-  //   historyApiFallback: true, //不跳转
-  //   inline: true, //实时刷新
-  //   hot: true  // 使用热加载插件 HotModuleReplacementPlugin
-  // }
+  devServer: {
+    proxy: {
+      // 凡是 `/api` 开头的 http 请求，都会被代理到 localhost:3000 上，由 koa 提供 mock 数据。
+      // koa 代码在 ./mock 目录中，启动命令为 npm run mock
+      '/api': {
+        target: 'http://localhost:3000',
+        secure: false
+      }
+    },
+    contentBase: "./public", //本地服务器所加载的页面所在的目录
+    historyApiFallback: true, //不跳转
+    inline: true, //实时刷新
+    hot: true  // 使用热加载插件 HotModuleReplacementPlugin
+  }
 
 };
